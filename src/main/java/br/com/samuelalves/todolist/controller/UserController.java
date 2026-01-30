4c3f82f0853f38f948178840ebecc03be4d7a80a
osamuelschen <schencknecht@discente.ufg.br>
2026-01-30 15:27:33 -0300

    Add hash and respondeentity

--- src/main/java/br/com/samuelalves/todolist/controller/UserController.java
+++ src/main/java/br/com/samuelalves/todolist/controller/UserController.java
@@ -1,10 +1,13 @@
 package br.com.samuelalves.todolist.controller;
 
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import br.com.samuelalves.todolist.model.UserModel;
 import br.com.samuelalves.todolist.repository.IUserRepository;
 import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,15 +21,20 @@ public class UserController {
     private IUserRepository iUserRepository;
 
     @PostMapping("/")
-    public UserModel createUser(@RequestBody UserModel userModel) {
+    public ResponseEntity createUser(@RequestBody UserModel userModel) {
         var user = this.iUserRepository.findByUsername(userModel.getUsername());
 
         if (user != null) {
-            System.out.println("Usuario já existente, tente outro username!");
-            return null;
+            //Status da Requisição com mensagem de erro
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
 
-        this.iUserRepository.save(userModel);
-        return userModel;
+        //Criptografia da senha
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHashred);
+
+        var userCreated = this.iUserRepository.save(userModel);
+        //Status da requisição
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
